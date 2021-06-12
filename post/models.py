cd4d5d5c1d6b4dbdaa8c68a03d9ee66a5e4b4350
BMWebApp2020 <info@aeromobix.com>
2021-06-12 19:39:40 +0530

    afd

--- post/models.py
+++ post/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    file = models.FileField(upload_to="PostImages/")
+    type = models.CharField(max_length=20)
+    no_of_like = models.IntegerField(default=0)
+    no_of_comments = models.IntegerField(default=0)
+    caption = models.CharField(max_length=500)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return user.username
--- post/views.py
+++ post/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth.models import User
 from .models import *
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url="/")
 def explore(request):
     return render(request,'home.html',{})
